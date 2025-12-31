48de5f4e53855f37b9627e159c5b8616d4dad380
Alex Nicholson <alex.n@clove.kitchen>
2025-12-31 11:37:15 +1100

    Add back withExpo() in next config

--- examples/next-16/next.config.ts
+++ examples/next-16/next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 import { withUniwind } from "uniwind-plugin-next";
+import { withExpo } from "@expo/next-adapter";
 
-const nextConfig: NextConfig = {
-  turbopack: {},
-};
+const nextConfig: NextConfig = {};
 
-export default withUniwind(nextConfig, {
+export default withUniwind(withExpo(nextConfig), {
   cssEntryFile: "./src/app/globals.css",
   extraThemes: ["ocean", "sunset"],
 });
